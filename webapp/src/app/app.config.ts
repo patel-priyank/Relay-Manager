dbd3edc69fefd36d2477015c59112106a31e8f9f
Priyank Patel <patel23priyank@gmail.com>
2026-04-05 15:50:29 +0100

    configure http client with api interceptor

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,14 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { providePrimeNG } from 'primeng/config';
 
 import Aura from '@primeuix/themes/aura';
 
+import { apiInterceptor } from './interceptors/api.interceptor';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([apiInterceptor])),
     providePrimeNG({
       theme: {
         preset: {
