cc3b84e68399d2836deb1db83bb41a53b3a209bf
Priyank Patel <patel23priyank@gmail.com>
2026-04-05 01:38:25 +0100

    initialize frontend

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { providePrimeNG } from 'primeng/config';
+
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+      ripple: true,
+    }),
+    provideRouter(routes),
+  ],
 };
