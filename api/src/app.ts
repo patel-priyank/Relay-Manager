947b6d45bf1b3d6385d1de0ab4b4446cb4cae28b
Priyank Patel <patel23priyank@gmail.com>
2026-05-04 15:48:51 +0100

    add request logging middleware

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 import cors from 'cors';
 
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction): void => {
+  console.log(req.method, req.path);
+  next();
+});
 
 app.get('/', (req: Request, res: Response): void => {
   res.json({ msg: 'API working.' });
