592d5ff4c1b68068e2d1b7dae2dbe375ad785b99
Priyank Patel <patel23priyank@gmail.com>
2026-05-05 19:27:14 +0100

    enable editing of alias label

--- webapp/src/app/components/alias-card/alias-card.ts
+++ webapp/src/app/components/alias-card/alias-card.ts
@@ -1,4 +1,14 @@
-import { Component, computed, effect, inject, input, output, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  inject,
+  input,
+  output,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -6,6 +16,7 @@ import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TagModule } from 'primeng/tag';
@@ -18,6 +29,7 @@ import { TagModule } from 'primeng/tag';
     CardModule,
     DialogModule,
     FormsModule,
+    InputTextModule,
     SelectButtonModule,
     SkeletonModule,
     TagModule,
@@ -26,6 +38,8 @@ import { TagModule } from 'primeng/tag';
   styleUrl: './alias-card.scss',
 })
 export class AliasCard {
+  @ViewChild('aliasLabelInputRef') aliasLabelInputRef!: ElementRef<HTMLInputElement>;
+
   alias = input<any | undefined>(undefined);
   isPremiumUser = input<boolean>(false);
 
@@ -58,6 +72,11 @@ export class AliasCard {
   ]);
 
   protected aliasDialogVisible = signal<boolean>(false);
+
+  protected aliasLabel = signal<string>('');
+  protected isAliasLabelEditable = signal<boolean>(false);
+  protected isSavingAliasLabel = signal<boolean>(false);
+
   protected blockingLevel = signal<string | undefined>(undefined);
   protected isSavingBlockingLevel = signal<boolean>(false);
 
@@ -66,20 +85,6 @@ export class AliasCard {
   constructor() {
     effect(() => {
       this.setBlockingLevel();
-    });
-  }
-
-  protected updateLabel() {
-    console.log('updateLabel() called');
-  }
-
-  protected copyAddress() {
-    navigator.clipboard.writeText(this.alias().full_address);
-
-    this.onShowMessage.emit({
-      success: true,
-      title: 'Success',
-      message: `Copied ${this.alias().full_address} to clipboard`,
     });
   }
 
@@ -95,6 +100,69 @@ export class AliasCard {
     } else {
       this.blockingLevel.set('all');
     }
+  }
+
+  protected showAliasLabelInput() {
+    this.isAliasLabelEditable.set(true);
+    this.aliasLabel.set(this.alias().description || '');
+    setTimeout(() => this.aliasLabelInputRef.nativeElement.focus());
+  }
+
+  protected updateAliasLabel() {
+    const apiKey = localStorage.getItem('relay-manager-api-key');
+
+    if (!this.alias()) return;
+    if (!apiKey) return;
+
+    let maskType = '';
+
+    switch (this.alias().mask_type) {
+      case 'random':
+        maskType = 'random';
+        break;
+
+      case 'custom':
+        maskType = 'domain';
+        break;
+    }
+
+    const body = { description: this.aliasLabel() };
+
+    this.isSavingAliasLabel.set(true);
+
+    this.http.patch(`/api/${maskType}/${this.alias().id}?token=${apiKey}`, body).subscribe({
+      next: (res: any) => {
+        this.isSavingAliasLabel.set(false);
+        this.isAliasLabelEditable.set(false);
+
+        this.onUpdateAlias.emit(res);
+
+        this.onShowMessage.emit({
+          success: true,
+          title: 'Success',
+          message: `Label updated for ${this.alias().full_address}`,
+        });
+      },
+      error: (_err: HttpErrorResponse) => {
+        this.isSavingAliasLabel.set(false);
+
+        this.onShowMessage.emit({
+          success: false,
+          title: 'Error',
+          message: `Label could not be updated for ${this.alias().full_address}`,
+        });
+      },
+    });
+  }
+
+  protected copyAddress() {
+    navigator.clipboard.writeText(this.alias().full_address);
+
+    this.onShowMessage.emit({
+      success: true,
+      title: 'Success',
+      message: 'Copied alias address to clipboard',
+    });
   }
 
   protected updateBlockingLevel(blockingLevel: string) {
@@ -160,7 +228,7 @@ export class AliasCard {
         this.onShowMessage.emit({
           success: true,
           title: 'Success',
-          message: `Blocking level updated to ${this.blockingLevels().find((level) => level.value === blockingLevel)?.longLabel} for ${this.alias().full_address}`,
+          message: `Blocking level updated for ${this.alias().full_address}`,
         });
       },
       error: (_err: HttpErrorResponse) => {
