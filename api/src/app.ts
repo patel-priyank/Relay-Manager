98684a5cd5e6a2cafcd3ac99bea37c97c10ce791
Priyank Patel <patel23priyank@gmail.com>
2026-05-14 10:09:15 +0100

    remove unused req variable in root route

--- api/src/app.ts
+++ api/src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response): void => {
+app.get('/', (_req: Request, res: Response): void => {
   res.json({ msg: 'API working.' });
 });
 
